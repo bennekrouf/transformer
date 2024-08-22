e875fcf4686b72b3a97b2600e7b84398ff1d21e2
Mohamed <mohamed.bennekrouf@gmail.com>
2024-08-22 11:55:42 +0200

    Add parameters in generated file

--- src/generate_yml_from_csv.rs
+++ src/generate_yml_from_csv.rs
@@ -5,7 +5,7 @@ use std::error::Error;
 use std::fs::{self, File};
 use std::path::Path;
 
-use crate::models::{Endpoint, Field, Entity, Property};
+use crate::models::{Endpoint, Field, Entity, Property, Parameter, Response};
 
 pub fn process_csv(input_file: &str, output_folder: &str) -> Result<(), Box<dyn Error>> {
     // Open the CSV file
@@ -59,25 +59,64 @@ pub fn process_csv(input_file: &str, output_folder: &str) -> Result<(), Box<dyn 
         Endpoint {
             path: "get_a_list_of_last_orders".to_string(),
             description: "Retrieve a list of last orders".to_string(),
+            parameters: vec![], // No parameters
+            response: Response {
+                r#type: "List of orders".to_string(),
+            },
         },
         Endpoint {
             path: "create_new_order".to_string(),
             description: "Create a new order".to_string(),
+            parameters: vec![Parameter {
+                name: "tenant_name".to_string(),
+                r#type: "string".to_string(),
+                description: Some("Name of the tenant to create".to_string()),
+                required: true,
+            }],
+            response: Response {
+                r#type: "Order confirmation".to_string(),
+            },
         },
         Endpoint {
             path: "update_order".to_string(),
             description: "Update an existing order".to_string(),
+            parameters: vec![Parameter {
+                name: "id".to_string(),
+                r#type: "string".to_string(),
+                description: Some("ID of the entity to update".to_string()),
+                required: true,
+            }],
+            response: Response {
+                r#type: "Update confirmation".to_string(),
+            },
         },
         Endpoint {
             path: "delete_order".to_string(),
             description: "Delete an order".to_string(),
+            parameters: vec![Parameter {
+                name: "id".to_string(),
+                r#type: "string".to_string(),
+                description: Some("ID of the entity to delete".to_string()),
+                required: true,
+            }],
+            response: Response {
+                r#type: "Deletion confirmation".to_string(),
+            },
         },
         Endpoint {
             path: "send_an_email_related_to_order".to_string(),
             description: "Send an email related to an order".to_string(),
+            parameters: vec![Parameter {
+                name: "email".to_string(),
+                r#type: "string".to_string(),
+                description: Some("Email address to send the notification to".to_string()),
+                required: true,
+            }],
+            response: Response {
+                r#type: "Email sent confirmation".to_string(),
+            },
         },
-   ];
-
+    ];
     // Create the output structure
     let output = Entity { endpoints, fields };
 
